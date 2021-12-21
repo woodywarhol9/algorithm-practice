9fc58cc3750dd00fafbaaacbe31807a7c69e62da
Woodywarhol9 <woodywarhol9@gmail.com>
2021-12-21 11:33:29 +0900

    dijk

---
+++ python_for_coding_test/part2/9.shortest_path/9-1.py
@@ -0,0 +1,65 @@
+"""
+간단한 다익스트라 알고리즘 : O(V^2)의 시간 복잡도, 노드의 개수가 5,000개 이하라면 활용!
+"""
+
+import sys
+input = sys.stdin.readline
+INF = int(1e9) # 무한 역할
+
+# 노드, 엣지 개수 입력
+n, m = map(int, input().split())
+# 시작 노드 번호 입력
+start = int(input())
+# 각 노드에 연결돼 있는 노드에 대한 정보 입력
+graph = [[] for i in range(n+1)]
+#방문한 적이 있는지 체크
+visited = [False] * (n + 1)
+#최단 거리 테이블 초기화
+distance = [INF] * (n + 1)
+
+#모든 엣지 정보 입력
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    #a번 노드에서 b번 노드로 가는 비용이 c라는 의미
+    graph[a].append((b,c))
+    
+#방문하지 않은 노드 중에서, 가장 최단 거리가 짧은 노드의 번호를 반환
+def get_smallest_node():
+    min_value = INF
+    index = 0 # 가장 최단 거리가 짧은 노드(index)
+    for i in range(1, n + 1):
+        if distance[i] < min_value and not visited[i]:
+            min_value = distance[i]
+            index = i
+    return index
+
+def dijkstra(start):
+    # 시작 노드에 대해서 초기화
+    distance[start] = 0
+    visited[start] = True
+    for j in graph[start]:
+        distance[j[0]] = j[1]
+    # 시작 노드를 제외한 전체 n - 1개의 노드에 대해 반복
+    for i in range(n-1):
+        # 현재 최단 거리가 가장 짧은 노드를 꺼내서, 방문 처리
+        now = get_smallest_node()
+        visited[now] = True
+        # 현재 노드와 연결된 다른 노드를 확인
+        for j in graph[now]:
+            cost = distance[now] + j[1]
+            #현재 노드를 거쳐서 다른 노드로 이동하는 거리가 더 짧은 경우
+            if cost < distance[j[0]]:
+                distance[j[0]] = cost
+                
+
+dijkstra(start)
+
+#모든 노드로 가기 위한 최단 거리를 출력
+for i in range(1, n + 1):
+    # 도달할 수 없는 경우, INF라고 출력
+    if distance[i] == INF:
+        print("INFINITY")
+    # 도달할 수 있는 경우 거리를 출력
+    else:
+        print(distance[i]) 
+
