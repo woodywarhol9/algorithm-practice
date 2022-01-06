abea7914fe657d6a995108c78622ea36606c0a32
Woodywarhol9 <woodywarhol9@gmail.com>
2022-01-06 22:52:09 +0900

    python_for_coding_test

--- python_for_coding_test/part3/13.dfs,bfs/13-19.py
+++ python_for_coding_test/part3/13.dfs,bfs/13-19.py
@@ -1,49 +1,46 @@
 """
-연산자 끼워 넣기
+연산자 끼워 넣기 : 다시 풀어보기.
 """
-from collections import deque 
 
-num_max = int(1e9)
-num_min = int(-1e9)
+n = int(input())
+# 연산을 수행하고자 하는 수 리스트
+data = list(map(int, input().split()))
+# 더하기, 빼기, 곱하기, 나누기 연산자 개수
+add, sub, mul, div = map(int, input().split())
 
-# 입력 받기
-N = int(input())
-num_list = list(map(int, input().split()))
-op_list = list(map(int, input().split()))
+# 최솟값과 최댓값 초기화
+min_value = int(1e9)
+max_value = int(-1e9)
 
-result = 0
-queue = deque(num_list)
-
-# 초기 q 설정
-count = 0
-while queue:
-    # 처음엔 queue에서 2개 꺼냄
-    if count == 0:
-        num1 = queue.popleft()
-        num2 = queue.popleft()
-    
+# DFS
+def dfs(i, now):
+    global min_value, max_value, add, sub, mul, div
+    # 모든 연산자를 다 사용한 경우, 최솟값과 최댓값 업데이트
+    if i == n:
+        min_value = min(min_value, now)
+        max_value = max(max_value, now)
     else:
-        num1 = result
-        num2 = queue.popleft()
-    
-    for i in range(4):
-        if i == 0 and op_list[0] > 0:
-            result = num1 + num2
-            op_list[0] -= 1
-            queue.append(result)
-        if i == 1 and op_list[1] > 0:
-            result = num1 - num2
-            op_list[1] -= 1
-            queue.append(result)
-        if i == 2 and op_list[2] > 0:
-            result = num1 * num2
-            op_list[2] -= 1
-            queue.append(result)
-        if i == 3 and op_list[3] > 0:
-            result = num // num2
-            op_list[3] -= 1
-            queue.append(result)
-        
-print(max(num_max, result))
-print(min(num_min, result))
+        # 각 연산자에 대하여 재귀적으로 수행
+        if add > 0:
+            add -= 1
+            dfs(i + 1, now + data[i])
+            add += 1
+        if sub > 0:
+            sub -= 1
+            dfs(i + 1, now - data[i])
+            sub += 1
+        if mul > 0:
+            mul -= 1
+            dfs(i + 1, now * data[i])
+            mul += 1
+        if div > 0:
+            div -= 1
+            dfs(i + 1, int(now / data[i])) # 나눌 때는 나머지를 제거
+            div += 1
 
+# DFS 메서드 호출
+dfs(1, data[0])
+
+# 최댓값과 최솟값 차례대로 출력
+print(max_value)
+print(min_value)
