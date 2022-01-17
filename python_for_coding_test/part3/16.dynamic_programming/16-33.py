27540663be21eb18ddab07db7377daa1efac4988
Woodywarhol9 <woodywarhol9@gmail.com>
2022-01-17 23:47:43 +0900

    python_for_codint_test

--- python_for_coding_test/part3/16.dynamic_programming/16-33.py
+++ python_for_coding_test/part3/16.dynamic_programming/16-33.py
@@ -1,8 +1,9 @@
 """
 퇴사 : 
 """
-n = int(input())
-dp = []
+n = int(input()) # 전체 상담 개수
+t = [] # 각 상담을 완료하는 데 걸리는 시간
+dp = [] # 각 상담을 완료했을 때 받을 수 있는 금액
 
 for _ in range(n):
     dp.append(list(map(int, input().split())))
