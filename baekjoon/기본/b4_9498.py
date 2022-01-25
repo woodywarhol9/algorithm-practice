3c9c3bc4b34bfbb500681b44322665cddd0e5f60
Woodywarhol9 <woodywarhol9@gmail.com>
2022-01-25 12:40:35 +0900

    baekjoon

---
+++ baekjoon/기본/b4_9498.py
@@ -0,0 +1,12 @@
+score = int(input())
+
+if 90 <= score and score <= 100:
+    print("A")
+elif 80 <= score and score <= 89:
+    print("B")
+elif 70 <= score and score <= 79:
+    print("C")
+elif 60 <= score and score <= 69:
+    print("D")
+else:
+    print("F") 
