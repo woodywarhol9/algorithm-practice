c200effb602d7c546d62cad8609e5c4c1837daaa
Woodywarhol9 <woodywarhol9@gmail.com>
2022-02-05 23:42:23 +0900

    baekjoon

--- baekjoon/기본/b1_4344.py
+++ baekjoon/기본/b1_4344.py
@@ -2,3 +2,9 @@ C = int(input())
 
 for _ in range(C):
     test_case = list(map(int, input().split()))
+    n, score_list = test_case[0], test_case[1:]
+    mean = sum(score_list) / n
+    score_list = [i for i in score_list if i > mean]
+    print(f'{len(score_list)*100 / n:.3f}%')
+    
+    
