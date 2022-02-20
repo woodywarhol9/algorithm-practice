4b8dbac86351fac97edfee1e303d8e82a6b4189e
Woodywarhol9 <woodywarhol9@gmail.com>
2022-02-20 21:59:02 +0900

    Update file

--- baekjoon/기본/b1_1157.py
+++ baekjoon/기본/b1_1157.py
@@ -1,5 +1,19 @@
-from collections import Counter
+word = input().upper()
+char = list(set(word))
 
-word = input()
-char_count = Counter(word.lower())
-print(max(char_count))
+cnt_list = []
+for c in char:
+    cnt = word.count(c)
+    cnt_list.append(cnt)
+
+if cnt_list.count(max(cnt_list)) > 1 :
+    print("?")
+
+else:
+    max_index = cnt_list.index(max(cnt_list))
+    print(char[max_index])
+    
+
+    
+
+    
