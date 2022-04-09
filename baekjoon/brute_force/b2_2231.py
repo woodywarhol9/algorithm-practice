7c555d78675066b0353c6ce9ec180474c39bf52c
Woodywarhol9 <woodywarhol9@gmail.com>
2022-04-09 23:59:21 +0900

    Update comment b2_2331.py

--- baekjoon/brute_force/b2_2231.py
+++ baekjoon/brute_force/b2_2231.py
@@ -1,20 +1,19 @@
+# 분해합 저장
 decompose_sum = {}
-
 for i in range(1, 10**6 + 1):
     sum = i
     for j in str(i):
         sum += int(j)
     decompose_sum[i] = sum 
 
+# 숫자 - 분해합 매칭
 sum_num = {}
 for k, v in decompose_sum.items():
     if v not in sum_num:
         sum_num[v] = k
         
 n = int(input())
-
 try:
     print(sum_num[n])
-
 except:
     print(0)
