743414324fabcd3c642b8cb6dafc02d6264de824
Woodywarhol9 <woodywarhol9@gmail.com>
2022-04-12 13:10:15 +0900

    Upload s5_1436.py

---
+++ baekjoon/brute_force/s5_1436.py
@@ -0,0 +1,14 @@
+n = int(input())
+start_num = 666
+count = 0
+
+while True:
+    if "666" in str(start_num):
+        count += 1
+    
+    if count == n:
+        break
+    start_num += 1
+print(start_num)
+    
+    
