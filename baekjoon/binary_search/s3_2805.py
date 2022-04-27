7c268bedcc15e74926be4ef1261289666875957a
Woodywarhol9 <woodywarhol9@gmail.com>
2022-04-27 11:41:37 +0900

    Add comment

--- baekjoon/binary_search/s3_2805.py
+++ baekjoon/binary_search/s3_2805.py
@@ -6,7 +6,7 @@ def binary_search(wood_h:list, target_h:int):
     while start <= end:
         # 자를 높이
         mid = (start + end) // 2
-        # 얻을 수 있는 나무 길이
+        # 얻을 수 있는 
         cut_h = sum([(i - mid) for i in wood_h if i > mid])
         # 높이를 더 늘려야 함
         if cut_h >= target_h:
@@ -17,9 +17,6 @@ def binary_search(wood_h:list, target_h:int):
     # 최대 높이
     print(end)
             
-        
-    
-
 wood_num, target_height = map(int, input().split())
 wood_height = list(map(int, sys.stdin.readline().rstrip().split()))
 binary_search(wood_height, target_height)
