efd1460b24db0799d83d3fed8ebf6fc3327dde21
Woodywarhol9 <woodywarhol9@gmail.com>
2022-05-19 15:10:05 +0900

    Comment : 주석 추가

--- softeer/3_스마트분류.py
+++ softeer/3_스마트분류.py
@@ -4,6 +4,7 @@ line_len, robot_range = map(int, input().split())
 line_info = list(input())
 
 count = 0
+# 앞에서부터 조회
 for idx, obj in enumerate(line_info):
     if obj == "P":
         for j in range(idx - robot_range, idx + robot_range + 1):
@@ -11,6 +12,7 @@ for idx, obj in enumerate(line_info):
                 continue
             elif line_info[j] == "H":
                 count += 1
+                # 다른 로봇이 사용하지 못하게 0으로 바꾸기.
                 line_info[j] = 0
                 break
 print(count)
