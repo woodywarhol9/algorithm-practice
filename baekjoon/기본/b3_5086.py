7a0998be1614c8023e82b512985f5a478a2dacda
Woodywarhol9 <woodywarhol9@gmail.com>
2022-06-08 10:16:25 +0900

    Add : 배수와 약수

---
+++ baekjoon/기본/b3_5086.py
@@ -0,0 +1,10 @@
+while True:
+    n, m = map(int, input().split())
+    if n == m == 0:
+        break
+    if m % n == 0:
+        print("factor")
+    elif n % m == 0:
+        print("multiple")
+    else:
+        print("neither")
